f1397c9cec4c67b1232b49cbcd4163645ebe3886
navI kuidyK <ikiduk2010@gmail.com>
2023-10-08 01:59:25 +0000

    task 11-12

--- javacore9exercises/src/main/java/com/navi/unit3/Main.java
+++ javacore9exercises/src/main/java/com/navi/unit3/Main.java
@@ -1,14 +1,18 @@
 package com.navi.unit3;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+// import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.navi.unit3.classes.Employee;
 import com.navi.unit3.classes.Greeter;
-import com.navi.unit3.classes.Measures;
-import com.navi.unit3.classes.interfaces.IntSequence;
-import com.navi.unit3.classes.interfaces.SquareSequence;
-import com.navi.unit3.classes.Threads;
+// import com.navi.unit3.classes.Measures;
+import com.navi.unit3.classes.Files;
+// import com.navi.unit3.classes.interfaces.IntSequence;
+// import com.navi.unit3.classes.interfaces.SquareSequence;
+// import com.navi.unit3.classes.Threads;
 
 public class Main {
 
@@ -24,48 +28,65 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Task 1
-        System.out.println("Avarage salary: " + Measures.avarage(employees));
+        // // Task 1
+        // System.out.println("Avarage salary: " + Measures.avarage(employees));
 
-        // Task 2
-        System.out.println(Measures.largest(employees).toString());
+        // // Task 2
+        // System.out.println(Measures.largest(employees).toString());
 
-        // Task 4
-        var sequence = IntSequence.of(2, 34345, 45346, 5, 6767, 8, 9, 654564567);
-        while(sequence.hasNext()) {
-        System.out.println(sequence.next());
+        // // Task 4
+        // var sequence = IntSequence.of(2, 34345, 45346, 5, 6767, 8, 9, 654564567);
+        // while(sequence.hasNext()) {
+        // System.out.println(sequence.next());
+        // }
+
+        // // Task 5
+        // var sequence1 = IntSequence.constant(5);
+        // while (sequence1.hasNext()) {
+        // System.out.println(sequence1.next());
+        // }
+
+        // // Task 6
+        // var sequence2 = new SquareSequence();
+        // int num = 0;
+        // while (sequence2.hasNext() && num <= 40) {
+        // System.out.println(sequence2.next());
+        // num++;
+        // }
+
+        // // Task 8
+        // var list = List.of("Helo", "HEllo", "a", "LALA888L");
+        // Measures.luckySort(list, (s1, s2) -> s1.length()-s2.length());
+
+        // // Task 9
+        // var threads = new ArrayList<Thread>();
+        // threads.add(new Thread(greeter1));
+        // threads.add(new Thread(greeter2));
+        // threads.add(new Thread(greeter3));
+        // threads.add(new Thread(greeter4));
+        // for (Thread t : threads) t.start();
+
+        // // Task 10.1
+        // Threads.runTogether(greeter1, greeter2, greeter3, greeter4);
+        // // Task 10.2
+        // Threads.runInOrder(greeter1, greeter2, greeter3, greeter4);
+
+        // // Task 11
+        var file = new File("javacore9exercises/src/main/java/com/navi/unit3/classes");
+        // try {
+        //     for (File f : Files.subdir(file, (f) -> f.isDirectory())) {
+        //         System.out.println(f.toString());
+        //     }
+        // } catch (IOException e) {
+        //     System.out.println(e.getMessage());
+        // }
+
+        // Task 12
+        try {
+        Files.listIncluding(file, "reads");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-
-        // Task 5
-        var sequence1 = IntSequence.constant(5);
-        while (sequence1.hasNext()) {
-            System.out.println(sequence1.next());
-        }
-
-        // Task 6
-        var sequence2 = new SquareSequence();
-        int num = 0;
-        while (sequence2.hasNext() && num <= 40) {
-            System.out.println(sequence2.next());
-            num++;
-        }
-
-        // Task 8
-        var list = List.of("Helo", "HEllo", "a", "LALA888L");
-        Measures.luckySort(list, (s1, s2) -> s1.length()-s2.length());
-
-        // Task 9
-        var threads = new ArrayList<Thread>();
-        threads.add(new Thread(greeter1));
-        threads.add(new Thread(greeter2));
-        threads.add(new Thread(greeter3));
-        threads.add(new Thread(greeter4));
-        for (Thread t : threads) t.start();
-
-        // Task 10.1
-        Threads.runTogether(greeter1, greeter2, greeter3, greeter4);
-        // Task 10.2
-        Threads.runInOrder(greeter1, greeter2, greeter3, greeter4);
 
     }
 
---
+++ javacore9exercises/src/main/java/com/navi/unit3/classes/Files.java
@@ -0,0 +1,46 @@
+package com.navi.unit3.classes;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Files {
+    public static ArrayList<File> subDir(File file, FileFilter filter) throws IOException {
+        if (file.isDirectory()) {
+            var output = new ArrayList<File>();
+            var list = file.listFiles(filter);
+            for (File f : list) {
+                output.add(f);
+            }
+            return output;
+        } else {
+            throw new IOException("The file is not a directory!");
+        }
+    }
+
+    public static ArrayList<File> subDirAll(File file) throws IOException {
+        return subDir(file, (f) -> true);
+    }
+
+    public static ArrayList<String> listIncluding(File file, FilenameFilter filter)
+            throws IOException {
+        if (file.isDirectory()) {
+            var output = new ArrayList<String>();
+            var list = file.list(filter);
+            for (String f : list) {
+                output.add(f);
+            }
+            return output;
+        } else {
+            throw new IOException("The file is not a directory!");
+        }
+    }
+
+    public static ArrayList<String> listIncluding(File file, String filter) throws IOException {
+        return listIncluding(file, (f, name) -> {
+            return name.contains(filter);
+        });
+    }
+}
